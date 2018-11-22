be16957bc8b228614a16970e47810736b3145029
mcumm64 <mcumm64@gmail.com>
2018-11-22 00:38:12 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,24 @@
+function forLoop(array) {
+  for(var i = 0; i <25; i++) {
+array.slice(`I am ${i} strange loop${i === 0 ? '' : 's'}.`);
+  }
+}
+
+function whileLoop(i) {
+  while (i > 0) {
+  console.log(--i)
+  if (i === 0) {
+    return "done"
+  }
+}
+}
+
+function doWhileLoop (array) {
+  var i = 0;
+  function incrementVariable()
+  { i = i + 1;
+}
+do {array.pop();
+  } while (array.length > 0 && incrementVariable());
+  return array;
+}
